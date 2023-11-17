0491f9f6710d38b02979c2708591cddde6d7a794
Júlia <juliadecastro777@gmail.com>
2023-11-17 10:20:03 -0300

    [Fix] Mudança no tipo do atributo
    Mudança na migration modalidade

--- src/database/migrations/20231116033459_modalidade.js
+++ src/database/migrations/20231116033459_modalidade.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable("modalidade", (table) => {
-    table.string("id").primary().unique().notNullable();
+    table.uuid("id").primary().unique().notNullable();
     table.string("nome").notNullable();
     table.enum("tipo", ['Olimpico', 'Paralimpico']).notNullable();
   });
