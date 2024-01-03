378a5318f87174a9a26105bb787244f6085cf10e
Júlia <juliadecastro777@gmail.com>
2024-01-03 15:30:20 -0300

    [Chore] Adicionando Logs em modalidade

--- src/controllers/Modalidade.js
+++ src/controllers/Modalidade.js
@@ -1,4 +1,5 @@
 const ModalidadeModel = require("../models/Modalidade");
+const LogsModel = require("../models/Logs");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
@@ -8,7 +9,17 @@ module.exports = {
       const modalidade = request.body;
       modalidade.id = uuidv4();
 
+      const log = {};
+      log.id = uuidv4();
+      log.responsavel = modalidade.responsavel;
+      log.data = new Date();
+      log.nomeTabela = "modalidade";
+      log.tabelaId = modalidade.id;
+      log.tipoAlteracao = "Create";
+      delete modalidade.responsavel;
+
       await ModalidadeModel.create(modalidade);
+      await LogsModel.create(log);
       return response.status(201).json({ id: modalidade.id });
     } catch (err) {
       console.error(`Modalidade creation failed: ${err}`);
@@ -47,10 +58,33 @@ module.exports = {
     try {
       const { id } = request.params;
       const modalidade = request.body;
+
+      const log = {};
+      log.id = uuidv4();
+      log.nomeTabela = "modalidade";
+      log.responsavel = modalidade.responsavel;
+      log.data = new Date();
+      log.tabelaId = id;
+      log.tipoAlteracao = "Update";
+      log.motivo = modalidade.motivo;
+
+      delete modalidade.responsavel;
+      delete modalidade.motivo;
+
       const stillExistFieldsToUpdate = Object.values(modalidade).length > 0;
       if (stillExistFieldsToUpdate) {
+        const atributos = Object.keys(modalidade);
+        const valoresNovos = Object.values(modalidade);
+        log.atributo = atributos.join(",");
+        const modalidadeAtual = await ModalidadeModel.getById(id);
+        const valoresAntigos = Object.fromEntries(atributos.map((chave) => [chave, modalidadeAtual[chave]]));
+        const valoresAntigosValues = Object.values(valoresAntigos);
+        log.valorAntigo = valoresAntigosValues.join(",");
+        log.novoValor = valoresNovos.join(",");
+
         await ModalidadeModel.updateById(id, modalidade);
-      }
+        await LogsModel.create(log);
+      } else return response.status(200).json("Não há dados para serem alterados");
       return response.status(200).json("OK");
     } catch (err) {
       console.error(`Modalidade update failed: ${err}`);
@@ -63,7 +97,18 @@ module.exports = {
   async delete(request, response) {
     try {
       const { id } = request.params;
+      const logData = request.body;
       await ModalidadeModel.deleteById(id);
+
+      const log = {};
+      log.id = uuidv4();
+      log.responsavel = logData.responsavel;
+      log.data = new Date();
+      log.nomeTabela = "modalidade";
+      log.tabelaId = id;
+      log.tipoAlteracao = "Delete";
+      log.motivo = logData.motivo;
+      await LogsModel.create(log);
       return response.status(200).json("OK");
     } catch (err) {
       console.error(`Modalidade delete failed: ${err}`);
