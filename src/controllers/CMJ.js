23a8a1c46c224a9d282e90415dbf2fc05fcbe1a0
Júlia <juliadecastro777@gmail.com>
2024-01-04 19:30:18 -0300

    [Chore] Adicionando logs no cmj
    Também foi adicionado a lógica de criar o teste geral a partir do cmj

--- src/controllers/CMJ.js
+++ src/controllers/CMJ.js
@@ -1,13 +1,16 @@
 const CMJModel = require("../models/CMJ");
-const { calcularMedia, calcularDesvioPadrao } = require("../utils/utilities");
+const { calcularMedia, calcularDesvioPadrao, pegaModalidade, calculaIdade } = require("../utils/utilities");
+const TesteModel = require("../models/Teste");
+const LogsModel = require("../models/Logs");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
+
+const idTipoTeste = 6;
 
 module.exports = {
   async create(request, response) {
     try {
       const cmj = request.body;
-      cmj.id = uuidv4();
       const valoresCMJ = [cmj.cmj1, cmj.cmj2, cmj.cmj3];
       cmj.mediaCmj = calcularMedia(valoresCMJ);
       cmj.desvPadCmj = calcularDesvioPadrao(valoresCMJ, cmj.mediaCmj);
@@ -15,8 +18,38 @@ module.exports = {
       cmj.minCmj = Math.min(...valoresCMJ);
       cmj.maxCmj = Math.max(...valoresCMJ);
 
-      await CMJModel.create(cmj);
-      return response.status(201).json({ id: cmj.id });
+      const log = {};
+      log.id = uuidv4();
+      log.responsavel = cmj.responsavel;
+      log.data = new Date();
+      log.nomeTabela = "cmj";
+      log.tipoAlteracao = "Create";
+      delete cmj.responsavel;
+
+      const teste = {};
+      teste.id = uuidv4();
+      teste.horaDaColeta = new Date();
+      teste.matriculaAtleta = cmj.matriculaAtleta;
+      teste.idTipoTeste = idTipoTeste;
+      teste.idModalidade = await pegaModalidade(cmj.matriculaAtleta);
+      teste.idade = await calculaIdade(cmj.matriculaAtleta);
+      await TesteModel.create(teste);
+      cmj.idTeste = teste.id;
+      log.tabelaId = teste.id;
+      delete cmj.matriculaAtleta;
+      delete cmj.idTipoTeste;
+
+      try {
+        await CMJModel.create(cmj);
+      } catch (err) {
+        console.error(`CMJ creation failed: ${err}`);
+        await TesteModel.deleteById(teste.id);
+        return response.status(500).json({
+          notification: "Internal server error"
+        });
+      }
+      await LogsModel.create(log);
+      return response.status(201).json({ id: cmj.idTeste });
     } catch (err) {
       console.error(`CMJ creation failed: ${err}`);
       return response.status(500).json({
@@ -80,10 +113,33 @@ module.exports = {
     try {
       const { idTeste } = request.params;
       const cmj = request.body;
+
+      const log = {};
+      log.id = uuidv4();
+      log.responsavel = cmj.responsavel;
+      log.data = new Date();
+      log.nomeTabela = "cmj";
+      log.tabelaId = idTeste;
+      log.tipoAlteracao = "Update";
+      log.motivo = cmj.motivo;
+
+      delete cmj.responsavel;
+      delete cmj.motivo;
+      const atributos = Object.keys(cmj);
+      const valoresNovos = Object.values(cmj);
+
+      const cmjAtual = await CMJModel.getByTeste(idTeste);
+      const valoresAntigos = Object.fromEntries(atributos.map((chave) => [chave, cmjAtual[0][chave]]));
+      const valoresAntigosValues = Object.values(valoresAntigos);
       const stillExistFieldsToUpdate = Object.values(cmj).length > 0;
+      log.atributo = atributos.join(",");
+      log.valorAntigo = valoresAntigosValues.join(",");
+      log.novoValor = valoresNovos.join(",");
+
       if (stillExistFieldsToUpdate) {
         await CMJModel.updateByTeste(idTeste, cmj);
-      }
+        await LogsModel.create(log);
+      } else return response.status(200).json("Não há dados a serem atualizados");
       return response.status(200).json("OK");
     } catch (err) {
       console.error(`CMJ update failed: ${err}`);
@@ -96,7 +152,20 @@ module.exports = {
   async delete(request, response) {
     try {
       const { idTeste } = request.params;
+      const logData = request.body;
+
+      const log = {};
+      log.id = uuidv4();
+      log.responsavel = logData.responsavel;
+      log.data = new Date();
+      log.nomeTabela = "cmj";
+      log.tabelaId = idTeste;
+      log.tipoAlteracao = "Delete";
+      log.motivo = logData.motivo;
+
       await CMJModel.deleteByTeste(idTeste);
+      await TesteModel.deleteById(idTeste);
+      await LogsModel.create(log);
       return response.status(200).json("OK");
     } catch (err) {
       console.error(`CMJ delete failed: ${err}`);
--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -8,10 +8,9 @@ module.exports = {
   },
 
   // Função para calcular o desvio padrão (recebe um array com os valores)
-  calcularDesvioPadrao: (arr) => {
-    const mean = this.calcularMedia(arr);
+  calcularDesvioPadrao: (arr, mean) => {
     const squaredDiffs = arr.map((num) => Math.pow(num - mean, 2));
-    const meanOfSquaredDiffs = calcularMedia(squaredDiffs);
+    const meanOfSquaredDiffs = squaredDiffs.reduce((acc, val) => acc + val, 0) / squaredDiffs.length;
     return Math.sqrt(meanOfSquaredDiffs);
   },
 
